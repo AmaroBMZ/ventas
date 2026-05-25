575fc86556e2502e2ad898ab071946a37c947394
ElTioBrian <bri.bravom@duocuc.cl>
2026-05-25 13:45:35 -0400

    validaciones completas

--- src/main/java/microservice/ventas/controller/VentaController.java
+++ src/main/java/microservice/ventas/controller/VentaController.java
@@ -3,11 +3,17 @@ package microservice.ventas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,36 +21,42 @@ import org.springframework.web.bind.annotation.RestController;
 import microservice.ventas.model.Venta;
 import microservice.ventas.service.VentaService;
 
-
 @RestController
-@RequestMapping("api/v1/venta")
+@RequestMapping("/api/v1/venta")
 public class VentaController {
 
     @Autowired
     private VentaService ventaService;
 
     @PostMapping
-    public Venta postVentas(@RequestBody Venta venta) {
-        return ventaService.crearVenta(venta);
+    public ResponseEntity<Venta> crearVenta(@Valid @RequestBody Venta venta) {
+        Venta nuevaVenta = ventaService.crearVenta(venta);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaVenta);
     }
-
     @GetMapping
-    public List<Venta> getVenta() {
-        return ventaService.obtenerVenta();
+    public ResponseEntity<List<Venta>> obtenerVentas() {
+        List<Venta> ventas = ventaService.obtenerVenta();
+        return ResponseEntity.ok(ventas);
     }
 
-    @GetMapping("{id}")
-    public Venta getVentaById(@PathVariable Long id) {
-        return ventaService.obtenerVentaPorId(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Venta> obtenerVentaPorId(@PathVariable Long id) {
+        Venta venta = ventaService.obtenerVentaPorId(id);
+
+        return ResponseEntity.ok(venta);
     }
 
-    @PutMapping("{id}")
-    public Venta putVenta(@PathVariable Long id, @RequestBody Venta venta) {
-        return ventaService.updateVenta(id, venta);
+    @PutMapping("/{id}")
+    public ResponseEntity<Venta> actualizarVenta(@PathVariable Long id,@Valid @RequestBody Venta venta) {
+        Venta ventaActualizada = ventaService.updateVenta(id, venta);
+        return ResponseEntity.ok(ventaActualizada);
     }
 
-    @DeleteMapping("{id}")
-    public void eliminarVenta(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarVenta(@PathVariable Long id) {
+
         ventaService.eliminarVenta(id);
+
+        return ResponseEntity.ok("Venta eliminada correctamente");
     }
 }
